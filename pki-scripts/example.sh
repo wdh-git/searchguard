79e609feafbd95e9ee806d2dc80c4596ae47cd22
wdh-001 <347954567@qq.com>
2016-06-07 17:35:13 +0800

    Update example.sh

--- pki-scripts/example.sh
+++ pki-scripts/example.sh
@@ -2,5 +2,5 @@
 set -e
 ./clean.sh
 ./gen_root_ca.sh capass changeit
-./gen_node_cert.sh 0 changeit capass 
-./gen_client_node_cert.sh admin changeit capass
+./gen_node_cert.sh node-0 changeit capass 
+./gen_client_node_cert.sh wdh-admin changeit capass
